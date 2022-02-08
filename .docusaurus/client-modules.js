b1ef2b7f43117a070d58efb474868a097ac7e63a
Navid <navid@netopia-system.com>
2022-02-08 11:45:13 +0200

    Remove docusaurus auto generated files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/var/www/html/Docusaurus/PaymentApi/node_modules/infima/dist/css/default/default.css"),
-  require("/var/www/html/Docusaurus/PaymentApi/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/var/www/html/Docusaurus/PaymentApi/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/var/www/html/Docusaurus/PaymentApi/src/css/custom.css"),
-];
